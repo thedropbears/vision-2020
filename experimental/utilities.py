ded1e889b937499ca5122940469db33662f04bc8
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-02-09 10:22:45 +1100

    Changed amount to number.

--- experimental/utilities.py
+++ experimental/utilities.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 
 
-def get_corners_from_contour(contour: np.ndarray, corner_amount=4) -> None:
+def get_corners_from_contour(contour: np.ndarray, corner_number=4) -> None:
     """Gets the corners of a contour.
 
     cv2 contours found using the cv2.findContours() method are imperfect and not helpful for
     use with tools like solvePNP. This method approximates the contour as a polygon with a specified
-    amount of corners/sides. It is more accurate than the previous version, as it uses the bisection method,
+    number of corners/sides. It is more accurate than the previous version, as it uses the bisection method,
     rather than just increasing or decreasing a number by a set amount.
 
     NOTE: The points are in clockwise or anticlockwise depending on the input contour. 
@@ -15,7 +15,7 @@ def get_corners_from_contour(contour: np.ndarray, corner_amount=4) -> None:
 
     Args:
         contour: a single contour in the format used by opencv.
-        corner_amount: The amount of corners the approximated polygon should have.
+        corner_number: The number of corners the approximated polygon should have.
 
     Returns:
         The approximated polygon, in a similar format to the 'contour' parameter.
@@ -29,14 +29,14 @@ def get_corners_from_contour(contour: np.ndarray, corner_amount=4) -> None:
     for _ in range(depth):
         current = (lower + upper) / 2
         hull = cv2.convexHull(cv2.approxPolyDP(contour, current, True))
-        if len(hull) > corner_amount:
+        if len(hull) > corner_number:
             lower = current
         else:
             upper = current
-    while len(hull) != corner_amount:
+    while len(hull) != corner_number:
         current = (lower + upper) / 2
         hull = cv2.convexHull(cv2.approxPolyDP(contour, current, True))
-        if len(hull) > corner_amount:
+        if len(hull) > corner_number:
             lower = current
         else:
             upper = current
