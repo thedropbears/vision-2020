9838cd50f8762f1209e3d1d047fe01d4cd924f9d
Sam Richards <35410487+Injector-Spenral@users.noreply.github.com>
2020-02-14 19:48:38 +1100

    Added functions to utilities file. (#7)
    * Added utilities file with contour approximation function. (#2)
    
    * Added utilities file with contour approximation function.
    
    * Changed amount to number.
    
    * Added limit to second loop to stop infinite while loop.
    
    * Added comments.
    
    * Lowered depth.
    
    * Added tests for contour approximation.
    
    * Moved utilities from experimental to utilities/functions.py.
    
    * Changed a test number.
    
    * Added scale_value() function to utilities. Copied directly from pyinfiniterecharge.
    
    * Added get_vertical_angle() and get_horizontal_angle() functions to utilities.
    
    * Added get_distance() function to utilities.
    
    * Added intrinsic matrix and distortion matrix to tests.py
    
    * Added test for get_distance().
    
    * Added test for get_angle functions.
    
    * Added check for whether exponent is necessary in scale_value()
    
    * Changed left to right in comment.
    
    * Clarified comment in functions.py
    
    * Removed unnecessary word from comment in functions.py

--- tests.py
+++ tests.py
@@ -2,8 +2,7 @@ import unittest
 import cv2
 import numpy as np
 from vision import Vision
-from utilities.functions import get_corners_from_contour
-
+from utilities.functions import *
 
 class VisionTests(unittest.TestCase):
     def test_sample_images(self):
@@ -16,6 +15,92 @@ class VisionTests(unittest.TestCase):
             if results is not None:
                 for i in range(1, len(values)):
                     self.assertAlmostEqual(results[i - 1], float(values[i]))
+
+class UtilitiesTests(unittest.TestCase):
+    TEST_INPUTS = np.array(
+        [
+            [
+                [[67, 40]],
+                [[161, 41]],
+                [[258, 43]],
+                [[238, 101]],
+                [[211, 160]],
+                [[179, 158]],
+                [[146, 151]],
+                [[122, 151]],
+                [[86, 146]],
+            ],
+            [
+                [[66, 65]],
+                [[92, 57]],
+                [[116, 50]],
+                [[134, 63]],
+                [[150, 79]],
+                [[151, 100]],
+                [[152, 121]],
+                [[100, 132]],
+                [[64, 116]],
+            ],
+        ]
+    )
+    TEST_OUTPUTS = np.array(
+        [
+            [[[67, 40]], [[258, 43]], [[211, 160]], [[86, 146]]],
+            [
+                [[66, 65]],
+                [[116, 50]],
+                [[152, 79]],
+                [[152, 121]],
+                [[100, 132]],
+                [[64, 116]],
+            ],
+        ]
+    )
+    INTR_MATRIX = np.array(
+        [[320, 0.0, 160], [0.0, 320, 120], [0.0, 0.0, 1.0]], dtype=np.float32
+    )
+
+    DIST_COEFFS = np.array(
+        [
+            [
+                1.27391079e-01,
+                -5.09404111e-01,
+                -7.87105714e-04,
+                2.60450896e-03,
+                1.04097100e00,
+            ]
+        ],
+        dtype=np.float32,
+    )
+        for inputs, outputs in zip(self.TEST_INPUTS, self.TEST_OUTPUTS):
+            self.assertTrue(
+                np.array_equal(
+                    sorted(
+                        list(get_corners_from_contour(inputs, len(outputs))),
+                        key=lambda x: x[0][0],
+                    ),
+                    sorted(list(outputs), key=lambda x: x[0][0]),
+                )
+            )
+
+    def test_get_distance(self):
+        self.assertAlmostEqual(1.0, get_distance(math.radians(-45), 2, 1, 0))
+        self.assertAlmostEqual(1.0, get_distance(0, 3, 2, math.radians(45)))
+        self.assertAlmostEqual(
+            1.0, get_distance(math.radians(-20), 3, 2, math.radians(25))
+        )
+        self.assertAlmostEqual(
+            math.sqrt(3), get_distance(math.radians(-10), 4, 3, math.radians(20))
+        )
+
+    def test_get_angles(self):
+        self.assertAlmostEqual(
+            math.radians(45), get_horizontal_angle(200, np.array([[100.0, 0.0, 100.0]]))
+        )
+        self.assertAlmostEqual(
+            math.radians(30),
+            get_vertical_angle(100.0, np.array([[], [0.0, 50 * math.sqrt(3), 50.0]])),
+        )
 
 
 class UtilitiesTests(unittest.TestCase):
@@ -73,5 +158,4 @@ class UtilitiesTests(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    camera_server = Vision(test=True)
     unittest.main()
--- utilities/functions.py
+++ utilities/functions.py
@@ -1,6 +1,42 @@
 import cv2
 import numpy as np
+import math
 
+
+def scale_value(
+    value: float,
+    input_lower: float,
+    input_upper: float,
+    output_lower: float,
+    output_upper: float,
+    exponent: float = 1,
+) -> float:
+    """Scales a value based on the input range and output range.
+    For example, to scale a joystick throttle (1 to -1) to 0-1, we would:
+        scale_value(joystick.getThrottle(), 1, -1, 0, 1)
+    The output is then raised to the exponent argument.
+
+    Args:
+        value: The value to be scaled.
+        input_lower: The `lower` end of the input range. Note that lower in this case
+            does not necessarily mean lower, just that it maps to the `lower` output.
+        input_upper: Similar to `input_lower`, but the other bound.
+        output_lower: The value which `input_lower` should scale to.
+        output_upper: The value which `input_upper` should scale to.
+        exponent (optional): The power to raise the result to.
+            Note that sign is preserved.
+    Returns:
+        The scaled value (es explained above).
+    """
+
+    input_distance = input_upper - input_lower
+    output_distance = output_upper - output_lower
+    ratio = (value - input_lower) / input_distance
+    result = ratio * output_distance + output_lower
+    if exponent == 1:
+        return result
+    else:
+        return math.copysign(result ** exponent, result)
 
 def get_corners_from_contour(contour: np.ndarray, corner_number=4) -> None:
     """Gets the corners of a contour.
@@ -53,3 +89,69 @@ def get_corners_from_contour(contour: np.ndarray, corner_number=4) -> None:
             upper = current
         i += 1
     return hull
+
+
+# The next two functions' `x` and `y` arguments are in camera coordinates.
+# The origin is the top left corner of the screen, with right and down being positive.
+
+# Both have an `invert` argument which, if False, leaves the return angle with negative
+# at the camera coordinates' origin, and if True, has positive at the camera coordinates' origin.
+
+# The intrinsic matrix should look like this:
+# np.array([
+#    [FX, 0.0, CX],
+#    [0.0, FY, CY],
+#    [0.0, 0.0, 1.0],
+# ])
+# Where FX and FY are the focal lengths in the horizontal and vertical
+# sizes of pixels, respectively, and CX and CY are the centres of the frame.
+# These values should be calculated by calibration, but can be calculated manually.
+
+
+def get_vertical_angle(y: int, intr_matrix: np.ndarray, inverted=False) -> float:
+    """Get the vertical angle of a point to the camera's centre.
+
+    Args:
+        y: The `y` of a point in camera coordinates. (As explained above)
+        intr_matrix: The camera's intrinsic properties (As explained above)
+        inverted: Inverts the output angle (As explained above)
+    Returns:
+        An angle, in radians.
+    """
+    if inverted:
+        return math.atan2(intr_matrix[1][2] - y, intr_matrix[1][1])
+    else:
+        return math.atan2(y - intr_matrix[1][2], intr_matrix[1][1])
+
+
+def get_horizontal_angle(x: int, intr_matrix: np.ndarray, inverted=False) -> float:
+    """Get the horizontal angle of a point to the camera's centre.
+
+    Args:
+        y: The `x` of a point in camera coordinates. (As explained above)
+        intr_matrix: The camera's intrinsic properties (As explained above)
+        inverted: Inverts the output angle (As explained above)
+    Returns:
+        An angle, in radians.
+    """
+    if inverted:
+        return math.atan2(intr_matrix[0][2] - x, intr_matrix[0][0])
+    else:
+        return math.atan2(x - intr_matrix[0][2], intr_matrix[0][0])
+
+
+def get_distance(
+    target_angle: float, target_height: float, camera_height: float, camera_tilt: float
+) -> None:
+    """Gets the ground distance from the camera to the target.
+
+    Args:
+        target_angle: The angle of the point below the centre plane of the camera. 
+            Downwards is positive, as returned by get_vertical_angle()'s default.
+        target_height: The height of the target above the ground (in metres)
+        camera_height: The height of the camera above the ground (in metres)
+        camera_tilt: The camera's angle of elevation from the ground (upwards is positive)
+    Returns:
+        A positive distance to the target along the ground (in metres)
+    """
+    return (target_height - camera_height) / math.tan(camera_tilt - target_angle)
