8ce35e1610605bd32540c02ff11c6fc06701c8d1
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-02-22 13:07:46 +1100

    Changed loading bay vision.py

--- loading-bay/vision.py
+++ loading-bay/vision.py
@@ -1,14 +1,17 @@
 from connection import Connection
 from camera_manager import CameraManager
+import cv2
+import numpy as np
 
 
 class Vision:
-    def __init__(self, connection: Connection, camera_manager: CameraManager):
+    def __init__(self, connection: Connection, camera_manager: CameraManager) -> None:
         self.camera_manager = camera_manager
+        self.connection = Connection
 
-    def run(self):
+    def run(self) -> None:
+        """Run the main vision loop once."""
         frame_time, self.frame = self.camera_manager.get_frame()
         if not frame_time:
             error = self.camera_manager.get_error()
             self.camera_manager.notify_error(error)
-
