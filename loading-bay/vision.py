991d0aebfcfba7200eef78f28a57ce543c8fe31b
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-02-22 13:16:36 +1100

    Added variable allocation

--- loading-bay/vision.py
+++ loading-bay/vision.py
@@ -2,12 +2,15 @@ from connection import Connection
 from camera_manager import CameraManager
 import cv2
 import numpy as np
+import time
 
 
 class Vision:
     def __init__(self, connection: Connection, camera_manager: CameraManager) -> None:
         self.camera_manager = camera_manager
-        self.connection = Connection
+        self.connection = connection
+
+        self.image = np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3), np.uint8)
 
     def run(self) -> None:
         """Run the main vision loop once."""
@@ -15,3 +18,14 @@ class Vision:
         if not frame_time:
             error = self.camera_manager.get_error()
             self.camera_manager.notify_error(error)
+            return
+
+        self.frame = cv2.rotate(self.frame, cv2.ROTATE_180, dst=self.frame)
+        results = self.get_image_values(self.frame)
+
+        if results is not None:
+            distance, angle = results
+            self.connection.send_results((distance, angle, time.monotonic()))
+
+    def get_image_values(self, frame: np.ndarray):
+        return None
