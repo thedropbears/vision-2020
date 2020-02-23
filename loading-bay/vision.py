0a84c484adb3e21fa936c4fa71340b11176fab2f
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-02-23 12:00:31 +1100

    Added test_contour_pair method

--- loading-bay/vision.py
+++ loading-bay/vision.py
@@ -39,3 +39,58 @@ class Vision:
     def get_image_values(self, frame: np.ndarray):
         self.image = self.frame
         return None
+
+    def test_contour_pair(self, inner: dict, outer: dict) -> bool:
+        """Test if a given contour pair is a valid loading bay.
+
+        Tests include (in order):
+            The number of points in the inner contour is greater than 3,
+            The number of points in the outer contour is greater than 3,
+            The ratio of the areas of the two contours,
+            The approximated inner contour has 4 sides,
+            The approximated outer contour has 4 sides,
+            The ratio of the areas of the inner contour and its approximated contour,
+            The ratio of the areas of the outer contour and its approximated contour,
+
+        Args:
+            inner: The inner contour (a dictionary (like how was created earlier)).
+            outer: The outer contour (a dictionary (like how was created earlier))
+        Returns:
+            A boolean of whether or not the pair is valid.
+            inner: The inner contour (with the rectangle added)
+            outer: The outer contour (with the rectangle and area added)
+        """
+
+        if not (inner["contour"].shape[0] > 3):
+            return False, inner, outer
+        if not (outer["contour"].shape[0] > 3):
+            return False, inner, outer
+
+        outer["area"] = cv2.contourArea(outer["contour"])
+
+        if not (
+            abs(outer["area"] / inner["area"] - INNER_OUTER_RATIO) < INNER_OUTER_ERROR
+        ):
+            return False, inner, outer
+
+        inner["rect"] = get_corners_from_contour(inner["contour"])
+
+        if not len(inner["rect"] == 4):
+            return False, inner, outer
+
+        outer["rect"] = get_corners_from_contour(outer["contour"])
+
+        if not len(outer["rect"] == 4):
+            return False, inner, outer
+
+        if not (
+            abs(inner["area"] / cv2.contourArea(inner["rect"]) - 1)
+            < RAW_RECT_AREA_ERROR
+        ):
+            return False, inner, outer
+        if not (
+            abs(outer["area"] / cv2.contourArea(outer["rect"]) - 1)
+            < RAW_RECT_AREA_ERROR
+        ):
+            return False, inner, outer
+        return True, inner, outer
