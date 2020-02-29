e059805c7cf81515b7290a3bbe113b85d6e7d7af
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-02-29 14:17:06 +1100

    Now uses argparse.

--- deploy.py
+++ deploy.py
@@ -1,34 +1,40 @@
 from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
-import sys
+import argparse
 
 files = ["magic_numbers.py", "connection.py", "camera_manager.py", "utilities"]
 
-try:
-    # Find where in the arguments `deploy.py` is.
-    # This is so it can be run as `python deploy.py etc` or `deploy.py etc`
-    for i, arg in enumerate(sys.argv[0:]):
-        if arg[-9:] == "deploy.py":
-            deploy_index = i
+parser = argparse.ArgumentParser(
+    "Upload code to the Pi. Only one option (power-port or loading-bay) can be used. IP defaults to 10.47.74.11"
+)
+parser.add_argument(
+    "-lb", "--loading-bay", help="Upload loading bay code", action="store_true"
+)
+parser.add_argument(
+    "-pp", "--power-port", help="Upload power port code", action="store_true"
+)
+parser.add_argument("-ip", "--ip", help="Specify a custom ip")
+args = parser.parse_args()
 
-    if sys.argv[deploy_index + 1] == "loading-bay":
-        main_file = "loading_bay_vision.py"
-        print("Deploying Loading Bay code")
 
-    elif sys.argv[deploy_index + 1] == "power-port":
-        main_file = "power_port_vision.py"
-        print("Deploying Power Port code")
+if args.loading_bay and args.power_port:
+    print(parser.print_help())
+    quit()
 
-    else:
-        quit()
-except:
-    print(
-        "Please ensure that you have `loading-bay` or `power-port` as the first argument"
-    )
+elif args.loading_bay:
+    main_file = "loading_bay_vision.py"
+    print("Deploying Loading Bay code")
+
+elif args.power_port:
+    main_file = "power_port_vision.py"
+    print("Deploying Power Port code")
+
+else:
+    parser.print_help()
     quit()
 
 
-server_ip = "10.47.74.11"
+server_ip = "10.47.74.11" if args.ip is None else args.ip
 username = "pi"
 password = "raspberry"
 
